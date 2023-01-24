a76c4ba1bef9addf29bc215c9c40f9d73dec9790
Aditya Mishra <95737654+maadi7@users.noreply.github.com>
2023-01-25 00:31:04 +0530

    Update constants.js

--- Context/constants.js
+++ Context/constants.js
@@ -1,6 +1,6 @@
 //0x5FbDB2315678afecb367f032d93F642f64180aa3
 import chatAppJSON from "./ChatApp.json";
 
-export const ChatAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const ChatAppAddress = "0x136C2033f2dD987f7386B09AF87b2f411286302d";
 
 export const ChatAppABI = chatAppJSON.abi;
